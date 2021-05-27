d5c5de4c04dbbfe6ce767a4810a20bb8d1d72b7f
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-05-27 14:11:02 -0400

    pull

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -17,10 +17,6 @@ function handleSubmit(e) {
   if(passwordRef.current.value !== passwordConfirmRef.current.value){
     return setError('passwords do not match')
 }
-<<<<<<< HEAD
-=======
-  
->>>>>>> 891936cdb30eb7b404f92aa00a597a3d47ba66b2
   try {
     setError('')
     setLoading(true)
@@ -29,14 +25,9 @@ function handleSubmit(e) {
     setError('Failed to create an account')
   }
   setLoading(false)
-<<<<<<< HEAD
 
 }
 
-=======
-}
-  
->>>>>>> 891936cdb30eb7b404f92aa00a597a3d47ba66b2
   return (
     <>
     <Card>
